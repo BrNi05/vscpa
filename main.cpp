fd4f941c6d52bb3160a67ca4235ce28872f81baf
BrNi05 <szocsbarnabas8@gmail.com>
2025-03-23 18:44:59 +0100

    Fixed args check

--- main.cpp
+++ main.cpp
@@ -3,25 +3,19 @@
 #include "./consts/consts.h"
 #include "./console/ui.h"
 #include "./io/io.h"
-#include "./devtools/memtrace.h"
-
-#include <thread>
-
 
 int main(int argc, char* argv[])
 {
     bool canExit = false;
     
     UI::setConsoleTitle(UI::CONSOLE_DEFAULT_TITLE);
-    
-    IO::fastSetup = IO::fastSetupExists(); // init
 
     /**
         * Checks for command line arguments (used for resetting fast setup)
         * If ran for first time with admin, add itself to PATH and setup
         * If ran for first time but with no admin rights, restart with elevated privileges
         * Else, start setup sequence
-    */
+    **/
     if (sysmod::firstRunWithAdmin())
     {
         sysmod::addSelfToPath();
@@ -35,22 +29,20 @@ int main(int argc, char* argv[])
     }
     else
     {
-        if (argc == 1)
+        if (argc == 2)
         {
             if (std::string(argv[1]) == Args::RESET)
             {
                 IO::resetFastSetup();
                 UI::infoMsg(UI::RESET_SUCCESS.data());
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                exit(0);
+                UI::exitDelayed(2);
             }
         }
         
         if (IO::startedFromFolder())
         {
             UI::infoMsg(UI::OPENED_FROM_EXPLORER.data());
-            std::this_thread::sleep_for(std::chrono::seconds(2));
-            exit(0);
+            UI::exitDelayed(2);
         }
 
         while (!canExit)
